43bfe12a5a0120c40aebd80accb922fba629dca1
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-12-06 17:33:29 +0530

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -60,7 +60,7 @@ def _watch(bot: Bot, update, isTar=False):
         return sendMessage(help_msg, bot, update)
 
     LOGGER.info(link)
-    listener = MirrorListener(bot, update, pswd, isTar)
+    listener = MirrorListener(bot, update, isTar, pswd=pswd, tag=tag)
     buttons = button_build.ButtonMaker()
     best_video = "bv*+ba/b"
     best_audio = "ba/b"
@@ -238,7 +238,7 @@ def watchTar(update, context):
 
 
 def watch(update, context):
-    _watch(context.bot, update, True)
+    _watch(context.bot, update)
 
 
 mirror_handler = CommandHandler(BotCommands.WatchCommand, watch,
