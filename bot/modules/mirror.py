a0c2cef3e16cbd5c8e81363316dded6d83228e89
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-12-09 20:44:47 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -157,9 +157,9 @@ class MirrorListener(listeners.MirrorListeners):
             msg = f'<b>☞ 📂Filename : </b><code>{download_dict[self.uid].name()}</code>\n<b>☞ 📦Size : </b><code>{size}</code>'
             if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                 msg += '\n<b>☞ 🌀Type : </b><code>Folder</code>'
-                msg += f'\n<b>☞ 🗳Powerd by : @AT_BOTs</b>'
+                msg += f'\n<b>☞ 🗳Powerd by : @Sathish_Sk_Offl</b>'
             else:
-                msg += f'\n<b>☞ 🗳Powerd by : @AT_BOTs</b>'
+                msg += f'\n<b>☞ 🗳Powerd by : @Sathish_Sk_Offl</b>'
             buttons = button_build.ButtonMaker()
             if SHORTENER is not None and SHORTENER_API is not None:
                 surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={link}&format=text').text
